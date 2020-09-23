ddff5ae699d562012a1b650ce6b94683086e6339
kaungsatkyaw <fired6559@gmail.com>
2020-09-23 16:43:28 +0630

    facebook messenger

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -14,6 +14,29 @@
     <div>
         @yield('content')
     </div>
+<!-- Load Facebook SDK for JavaScript -->
+      <div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v8.0'
+          });
+        };
+
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+      <!-- Your Chat Plugin code -->
+      <div class="fb-customerchat"
+        attribution=setup_tool
+        page_id="463959887143269">
+      </div>
 </body>
 <script>
 	function openNav() {
@@ -42,4 +65,5 @@
     document.documentElement.scrollTop = 0;
   }
 </script>
+
 </html>
